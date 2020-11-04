10faad48ada372bae9befe9a00d85ad65c0102eb
JK012266 <jurikovalevski@gmail.com>
2020-11-04 20:36:47 +0200

    Initial commit

---
+++ src/com/company/BRAVO.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BRAVO {
+    public static void main(String[] args) {
+        int a = 2000000000;// tulemus -294967296
+        System.out.println(a + a);
+        float b = 1.89f;
+        System.out.print(b + 11);// 12.89
+    }
+}
